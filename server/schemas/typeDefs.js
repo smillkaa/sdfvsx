d6f04ad381faf537178b103c9505ec1f94272b53
smillkaa <104727784+smillkaa@users.noreply.github.com>
2022-10-09 21:55:31 -0700

    added resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,46 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+
     type Query {
-        helloworld: String
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+
+    type Mutation {
+        login(email: String!, password:String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: saveBookInput): User
+        removeBook(bookId: String!): User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+
+        bookId: ID
+        title: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
 `;
+
+
 
 // export the typeDefs
 module.exports = typeDefs;
