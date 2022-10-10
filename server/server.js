089d69133bff505fe34b049716069374a2aa638c
smillkaa <104727784+smillkaa@users.noreply.github.com>
2022-10-09 21:57:11 -0700

    added context to server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req }) => req.headers
 });
 
 app.use(express.urlencoded({ extended: true }));
